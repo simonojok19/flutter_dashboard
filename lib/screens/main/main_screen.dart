0c4f38d68fa49fce820e2b3c82658fd910ee85b2
simonojok19 <simonojok19@gmail.com>
2021-06-19 16:47:36 +0300

    creating the layouts for the pages

--- lib/screens/main/main_screen.dart
+++ lib/screens/main/main_screen.dart
@@ -3,6 +3,24 @@ import 'package:flutter/material.dart';
 class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Row(
+          children: [
+            Expanded(
+              child: Container(
+                color: Colors.white,
+              ),
+            ),
+            Expanded(
+              flex: 5,
+              child: Container(
+                color: Colors.blue,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
