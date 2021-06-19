d55d02503dc4d46222c8b38e1edfa27de0844eda
simonojok19 <simonojok19@gmail.com>
2021-06-19 23:23:06 +0300

    finishing the card

---
+++ lib/screens/dashboard/components/storage_details.dart
@@ -0,0 +1,70 @@
+import 'package:admin/screens/dashboard/components/storage_info_card.dart';
+import 'package:flutter/material.dart';
+
+import '../../../constants.dart';
+
+class StorageDetail extends StatelessWidget {
+  const StorageDetail({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(
+        defaultPadding,
+      ),
+      decoration: BoxDecoration(
+        color: secondaryColor,
+        borderRadius: const BorderRadius.all(
+          Radius.circular(
+            10,
+          ),
+        ),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "Storage Details",
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          SizedBox(
+            height: defaultPadding,
+          ),
+          SizedBox(
+            height: 200,
+            child: Chart(),
+          ),
+          StorageInfoCard(
+            svgSrc: "assets/icons/Documents.svg",
+            title: "Document Files",
+            files: "1.3 GB",
+            numFiles: 1328,
+          ),
+          StorageInfoCard(
+            svgSrc: "assets/icons/media.svg",
+            title: "Media Files",
+            files: "15 GB",
+            numFiles: 108,
+          ),
+          StorageInfoCard(
+            svgSrc: "assets/icons/folder.svg",
+            title: "Other Files",
+            files: "8.3 GB",
+            numFiles: 1328,
+          ),
+          StorageInfoCard(
+            svgSrc: "assets/icons/unknown.svg",
+            title: "Unknown",
+            files: "10 MB",
+            numFiles: 140,
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/screens/dashboard/dashboard_screen.dart
+++ lib/screens/dashboard/dashboard_screen.dart
@@ -5,6 +5,7 @@ import 'package:flutter_svg/flutter_svg.dart';
 
 import 'components/chart.dart';
 import 'components/header.dart';
+import 'components/storage_details.dart';
 import 'components/storage_info_card.dart';
 
 class DashboardScreen extends StatelessWidget {
@@ -38,62 +39,7 @@ class DashboardScreen extends StatelessWidget {
                 ),
                 Expanded(
                   flex: 2,
-                  child: Container(
-                    padding: EdgeInsets.all(
-                      defaultPadding,
-                    ),
-                    decoration: BoxDecoration(
-                      color: secondaryColor,
-                      borderRadius: const BorderRadius.all(
-                        Radius.circular(
-                          10,
-                        ),
-                      ),
-                    ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          "Storage Details",
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.w500,
-                          ),
-                        ),
-                        SizedBox(
-                          height: defaultPadding,
-                        ),
-                        SizedBox(
-                          height: 200,
-                          child: Chart(),
-                        ),
-                        StorageInfoCard(
-                          svgSrc: "assets/icons/Documents.svg",
-                          title: "Document Files",
-                          files: "1.3 GB",
-                          numFiles: 1328,
-                        ),
-                        StorageInfoCard(
-                          svgSrc: "assets/icons/media.svg",
-                          title: "Media Files",
-                          files: "15 GB",
-                          numFiles: 108,
-                        ),
-                        StorageInfoCard(
-                          svgSrc: "assets/icons/folder.svg",
-                          title: "Other Files",
-                          files: "8.3 GB",
-                          numFiles: 1328,
-                        ),
-                        StorageInfoCard(
-                          svgSrc: "assets/icons/unknown.svg",
-                          title: "Unknown",
-                          files: "10 MB",
-                          numFiles: 140,
-                        ),
-                      ],
-                    ),
-                  ),
+                  child: StorageDetail(),
                 ),
               ],
             ),
