1f71d036561f457bae49f5bb2ba4728815b23fb8
simonojok19 <simonojok19@gmail.com>
2021-06-19 21:27:59 +0300

    removed un used imports

--- lib/screens/dashboard/dashboard_screen.dart
+++ lib/screens/dashboard/dashboard_screen.dart
@@ -1,8 +1,7 @@
 import 'package:admin/constants.dart';
-import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
+import 'components/chart.dart';
 import 'components/header.dart';
 
 
