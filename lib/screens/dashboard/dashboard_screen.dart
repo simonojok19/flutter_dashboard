54679ef3e280be4b4731eeaae64fd648d2aacf5e
simonojok19 <simonojok19@gmail.com>
2021-06-19 19:32:39 +0300

    refactored the codes into header component

--- lib/screens/dashboard/dashboard_screen.dart
+++ lib/screens/dashboard/dashboard_screen.dart
@@ -2,6 +2,8 @@ import 'package:admin/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
+import 'components/header.dart';
+
 class DashboardScreen extends StatelessWidget {
   const DashboardScreen({Key key}) : super(key: key);
 
@@ -12,88 +14,8 @@ class DashboardScreen extends StatelessWidget {
         padding: EdgeInsets.all(defaultPadding),
         child: Column(
           children: [
-            Row(
-              children: [
-                Text(
-                  "Dashboard",
-                  style: Theme.of(context).textTheme.headline6,
-                ),
-                Spacer(),
-                Expanded(child: SearchField(),),
-                Container(
-                  margin: EdgeInsets.only(left: defaultPadding),
-                  padding: EdgeInsets.symmetric(
-                    horizontal: defaultPadding,
-                    vertical: defaultPadding / 2,
-                  ),
-                  decoration: BoxDecoration(
-                    color: secondaryColor,
-                    borderRadius: BorderRadius.all(Radius.circular(10,),),
-                    border: Border.all(
-                      color: Colors.white10,
-                    ),
-                  ),
-                  child: Row(
-                    children: [
-                      Image.asset("assets/images/profile_pic.png", height: 38,),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: defaultPadding / 2,
-                        ),
-                        child: Text("Abbo Rachael"),
-                      ),
-                      Icon(Icons.keyboard_arrow_down,),
-                    ],
-                  ),
-                )
-              ],
-            )
+            Header()
           ],
-        ),
-      ),
-    );
-  }
-}
-
-class SearchField extends StatelessWidget {
-  const SearchField({
-    Key key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return TextField(
-      decoration: InputDecoration(
-        fillColor: secondaryColor,
-        filled: true,
-        border: OutlineInputBorder(
-          borderSide: BorderSide.none,
-          borderRadius: const BorderRadius.all(
-            Radius.circular(
-              10,
-            ),
-          ),
-        ),
-        suffixIcon: InkWell(
-          onTap: () {},
-          child: Container(
-            padding: EdgeInsets.all(
-              defaultPadding * 0.75,
-            ),
-            margin: EdgeInsets.symmetric(
-              horizontal: defaultPadding / 2,
-            ),
-            decoration: BoxDecoration(
-                color: primaryColor,
-                borderRadius: BorderRadius.all(
-                  Radius.circular(
-                    10,
-                  ),
-                )),
-            child: SvgPicture.asset(
-              "assets/icons/Search.svg",
-            ),
-          ),
         ),
       ),
     );
