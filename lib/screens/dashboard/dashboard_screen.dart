6b41a4517a656bcc79e8e76c76d5c794419859e9
simonojok19 <simonojok19@gmail.com>
2021-06-19 18:50:53 +0300

    exported search field

--- lib/screens/dashboard/dashboard_screen.dart
+++ lib/screens/dashboard/dashboard_screen.dart
@@ -20,39 +20,56 @@ class DashboardScreen extends StatelessWidget {
                 ),
                 Spacer(),
                 Expanded(
-                  child: TextField(
-                    decoration: InputDecoration(
-                      fillColor: secondaryColor,
-                      filled: true,
-                      border: OutlineInputBorder(
-                        borderSide: BorderSide.none,
-                        borderRadius: const BorderRadius.all(
-                          Radius.circular(
-                            10,
-                          ),
-                        ),
-                      ),
-                      suffixIcon: Container(
-                        padding: EdgeInsets.all(
-                          defaultPadding * 0.75,
-                        ),
-                        decoration: BoxDecoration(
-                            color: primaryColor,
-                            borderRadius: BorderRadius.all(
-                              Radius.circular(
-                                10,
-                              ),
-                            )),
-                        child: SvgPicture.asset(
-                          "assets/icons/Search.svg",
-                        ),
-                      ),
-                    ),
-                  ),
+                  child: SearchField(),
                 ),
               ],
             )
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class SearchField extends StatelessWidget {
+  const SearchField({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      decoration: InputDecoration(
+        fillColor: secondaryColor,
+        filled: true,
+        border: OutlineInputBorder(
+          borderSide: BorderSide.none,
+          borderRadius: const BorderRadius.all(
+            Radius.circular(
+              10,
+            ),
+          ),
+        ),
+        suffixIcon: InkWell(
+          onTap: () {},
+          child: Container(
+            padding: EdgeInsets.all(
+              defaultPadding * 0.75,
+            ),
+            margin: EdgeInsets.symmetric(
+              horizontal: defaultPadding / 2,
+            ),
+            decoration: BoxDecoration(
+                color: primaryColor,
+                borderRadius: BorderRadius.all(
+                  Radius.circular(
+                    10,
+                  ),
+                )),
+            child: SvgPicture.asset(
+              "assets/icons/Search.svg",
+            ),
+          ),
         ),
       ),
     );
