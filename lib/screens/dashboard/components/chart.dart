72fa8929031c77ee1d3f62ada7e4bbfe00788060
simonojok19 <simonojok19@gmail.com>
2021-06-19 23:11:04 +0300

    finished the sidebar

--- lib/screens/dashboard/components/chart.dart
+++ lib/screens/dashboard/components/chart.dart
@@ -56,6 +56,9 @@ class Chart extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
+              SizedBox(
+                height: defaultPadding,
+              ),
               Text(
                 "29.1",
                 style: Theme.of(context).textTheme.headline4.copyWith(
--- lib/screens/dashboard/dashboard_screen.dart
+++ lib/screens/dashboard/dashboard_screen.dart
@@ -1,9 +1,10 @@
 import 'package:admin/constants.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 import 'components/chart.dart';
 import 'components/header.dart';
-
 
 class DashboardScreen extends StatelessWidget {
   const DashboardScreen({Key key}) : super(key: key);
@@ -38,7 +39,6 @@ class DashboardScreen extends StatelessWidget {
                     padding: EdgeInsets.all(
                       defaultPadding,
                     ),
-                    height: 500,
                     decoration: BoxDecoration(
                       color: secondaryColor,
                       borderRadius: const BorderRadius.all(
@@ -63,6 +63,30 @@ class DashboardScreen extends StatelessWidget {
                           height: 200,
                           child: Chart(),
                         ),
+                        StorageInfoCard(
+                          svgSrc: "assets/icons/Documents.svg",
+                          title: "Document Files",
+                          files: "1.3 GB",
+                          numFiles: 1328,
+                        ),
+                        StorageInfoCard(
+                          svgSrc: "assets/icons/media.svg",
+                          title: "Media Files",
+                          files: "15 GB",
+                          numFiles: 108,
+                        ),
+                        StorageInfoCard(
+                          svgSrc: "assets/icons/folder.svg",
+                          title: "Other Files",
+                          files: "8.3 GB",
+                          numFiles: 1328,
+                        ),
+                        StorageInfoCard(
+                          svgSrc: "assets/icons/unknown.svg",
+                          title: "Unknown",
+                          files: "10 MB",
+                          numFiles: 140,
+                        ),
                       ],
                     ),
                   ),
@@ -71,6 +95,78 @@ class DashboardScreen extends StatelessWidget {
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class StorageInfoCard extends StatelessWidget {
+  final String title;
+  final String svgSrc;
+  final files;
+  final int numFiles;
+
+  const StorageInfoCard({
+    Key key,
+    @required this.title,
+    @required this.svgSrc,
+    @required this.files,
+    @required this.numFiles,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: defaultPadding,),
+      padding: EdgeInsets.all(
+        defaultPadding,
+      ),
+      decoration: BoxDecoration(
+        border: Border.all(
+          width: 2,
+          color: primaryColor.withOpacity(
+            0.15,
+          ),
+        ),
+        borderRadius: BorderRadius.all(
+          Radius.circular(
+            defaultPadding,
+          ),
+        ),
+      ),
+      child: Row(
+        children: [
+          SizedBox(
+            height: 20,
+            width: 20,
+            child: SvgPicture.asset(
+              svgSrc,
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: defaultPadding),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    title,
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  Text(
+                    "$numFiles Files",
+                    style: Theme.of(context)
+                        .textTheme
+                        .caption
+                        .copyWith(color: Colors.white70),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Text(files)
+        ],
       ),
     );
   }
