ced476808a3762015abdcc849fe0135ca70a1273
simonojok19 <simonojok19@gmail.com>
2021-06-19 20:37:02 +0300

    adding the graphs

--- lib/screens/dashboard/dashboard_screen.dart
+++ lib/screens/dashboard/dashboard_screen.dart
@@ -16,11 +16,45 @@ class DashboardScreen extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Header(),
+            SizedBox(
+              height: defaultPadding,
+            ),
             Row(
               children: [
                 Expanded(
+                  flex: 5,
                   child: Container(
+                    height: 500,
                     color: Colors.white,
+                  ),
+                ),
+                SizedBox(
+                  width: defaultPadding,
+                ),
+                Expanded(
+                  flex: 2,
+                  child: Container(
+                    padding: EdgeInsets.all(
+                      defaultPadding,
+                    ),
+                    height: 500,
+                    decoration: BoxDecoration(
+                      color: secondaryColor,
+                      borderRadius: const BorderRadius.all(
+                        Radius.circular(
+                          10,
+                        ),
+                      ),
+                    ),
+                    child: Column(
+                      children: [
+                        Text(
+                          "Storage Details",
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.w500),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ],
