8d3de8347634b3fa2823b5d064467c12af69e354
simonojok19 <simonojok19@gmail.com>
2021-06-20 12:43:57 +0300

    refactored all components

---
+++ lib/screens/dashboard/components/rescent_files.dart
@@ -0,0 +1,110 @@
+import 'package:admin/models/RecentFile.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../../../constants.dart';
+import 'my_files.dart';
+
+class RecentFiles extends StatelessWidget {
+  const RecentFiles({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        MyFiles(),
+        SizedBox(
+          height: defaultPadding,
+        ),
+        Container(
+          padding: EdgeInsets.all(
+            defaultPadding,
+          ),
+          decoration: BoxDecoration(
+            color: secondaryColor,
+            borderRadius: const BorderRadius.all(
+              Radius.circular(
+                10,
+              ),
+            ),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Recent Files",
+                style: Theme.of(context).textTheme.subtitle1,
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: DataTable(
+                  horizontalMargin: 0,
+                  columnSpacing: defaultPadding,
+                  columns: [
+                    DataColumn(
+                      label: Text(
+                        "File Name",
+                      ),
+                    ),
+                    DataColumn(
+                      label: Text(
+                        "Date",
+                      ),
+                    ),
+                    DataColumn(
+                      label: Text(
+                        "Size",
+                      ),
+                    ),
+                  ],
+                  rows: List.generate(
+                    demoRecentFiles.length,
+                        (index) => recentFileDataRow(
+                      demoRecentFiles[index],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+DataRow recentFileDataRow(RecentFile file) {
+  return DataRow(
+    cells: [
+      DataCell(
+        Row(
+          children: [
+            SvgPicture.asset(
+              file.icon,
+              height: 30,
+              width: 30,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: defaultPadding,
+              ),
+              child: Text(file.title,),
+            ),
+          ],
+        ),
+      ),
+      DataCell(
+        Text(
+          file.date,
+        ),
+      ),
+      DataCell(
+        Text(
+          file.size,
+        ),
+      ),
+    ],
+  );
+}
