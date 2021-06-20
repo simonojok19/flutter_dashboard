a4d4ffccb5f71fca5c53714af8e891a4be43c6ef
simonojok19 <simonojok19@gmail.com>
2021-06-20 13:06:37 +0300

    undone for errors

--- lib/screens/dashboard/dashboard_screen.dart
+++ lib/screens/dashboard/dashboard_screen.dart
@@ -1,9 +1,11 @@
 import 'package:admin/constants.dart';
+import 'package:admin/models/RecentFile.dart';
 import 'package:admin/responsive.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'components/header.dart';
+import 'components/my_files.dart';
 import 'components/rescent_files.dart';
 import 'components/storage_details.dart';
 
@@ -28,7 +30,66 @@ class DashboardScreen extends StatelessWidget {
               children: [
                 Expanded(
                   flex: 5,
-                  child: RecentFiles(),
+                  child: Column(
+                    children: [
+                      MyFiles(),
+                      SizedBox(
+                        height: defaultPadding,
+                      ),
+                      Container(
+                        padding: EdgeInsets.all(
+                          defaultPadding,
+                        ),
+                        decoration: BoxDecoration(
+                          color: secondaryColor,
+                          borderRadius: const BorderRadius.all(
+                            Radius.circular(
+                              10,
+                            ),
+                          ),
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Recent Files",
+                              style: Theme.of(context).textTheme.subtitle1,
+                            ),
+                            SizedBox(
+                              width: double.infinity,
+                              child: DataTable(
+                                horizontalMargin: 0,
+                                columnSpacing: defaultPadding,
+                                columns: [
+                                  DataColumn(
+                                    label: Text(
+                                      "File Name",
+                                    ),
+                                  ),
+                                  DataColumn(
+                                    label: Text(
+                                      "Date",
+                                    ),
+                                  ),
+                                  DataColumn(
+                                    label: Text(
+                                      "Size",
+                                    ),
+                                  ),
+                                ],
+                                rows: List.generate(
+                                  demoRecentFiles.length,
+                                      (index) => recentFileDataRow(
+                                    demoRecentFiles[index],
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
                 if (!Responsive.isMobile(
                   context,
