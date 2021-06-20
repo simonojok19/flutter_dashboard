45b1ee194e211346b159f159d42999fb69b13a03
simonojok19 <simonojok19@gmail.com>
2021-06-20 11:11:30 +0300

    refactored the file info

--- lib/screens/dashboard/components/my_files.dart
+++ lib/screens/dashboard/components/my_files.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 import '../../../constants.dart';
+import 'file_info_card.dart';
 
 class MyFiles extends StatelessWidget {
   const MyFiles({
@@ -43,118 +44,10 @@ class MyFiles extends StatelessWidget {
           gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
             crossAxisCount: 4,
             crossAxisSpacing: defaultPadding,
+            childAspectRatio: 1.4
           ),
           itemBuilder: (context, index) => FileInfoCard(
             info: demoMyFiels[index],
-          ),
-        ),
-      ],
-    );
-  }
-}
-
-class FileInfoCard extends StatelessWidget {
-  final CloudStorageInfo info;
-  const FileInfoCard({
-    Key key,
-    @required this.info,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(defaultPadding),
-      decoration: BoxDecoration(
-        color: secondaryColor,
-        borderRadius: const BorderRadius.all(
-          Radius.circular(
-            10,
-          ),
-        ),
-      ),
-      child: Column(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Container(
-                padding: EdgeInsets.all(
-                  defaultPadding * 0.75,
-                ),
-                height: 40,
-                width: 40,
-                decoration: BoxDecoration(
-                  color: info.color.withOpacity(0.1),
-                  borderRadius: const BorderRadius.all(
-                    Radius.circular(
-                      10,
-                    ),
-                  ),
-                ),
-                child: SvgPicture.asset(
-                  info.svgSrc,
-                  color: info.color,
-                ),
-              ),
-              Icon(
-                Icons.more_vert,
-                color: Colors.white54,
-              )
-            ],
-          ),
-          Text(
-            info.title,
-            maxLines: 1,
-            overflow: TextOverflow.ellipsis,
-          ),
-          ProgressLine(
-            color: info.color,
-            percentage: info.percentage,
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class ProgressLine extends StatelessWidget {
-  const ProgressLine({
-    Key key,
-    this.color = primaryColor,
-    @required this.percentage,
-  }) : super(key: key);
-
-  final Color color;
-  final int percentage;
-
-  @override
-  Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Container(
-          width: double.infinity,
-          height: 5,
-          decoration: BoxDecoration(
-            color: color.withOpacity(0.1),
-            borderRadius: BorderRadius.all(
-              Radius.circular(
-                10,
-              ),
-            ),
-          ),
-        ),
-        LayoutBuilder(
-          builder: (context, constraint) => Container(
-            width: constraint.maxWidth * ( percentage / 100 ),
-            height: 5,
-            decoration: BoxDecoration(
-              color: color,
-              borderRadius: BorderRadius.all(
-                Radius.circular(
-                  10,
-                ),
-              ),
-            ),
           ),
         ),
       ],
